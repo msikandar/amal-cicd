fcd3ff126b5114c8e73551f44387011bee6cfb52
abu-hanzla <hanzla611@gmail.com>
2024-06-10 16:30:56 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-         Hello World - 
+         Hello World - test
         </div>
 
         <div className={styles.grid}>
