2ee8d782337e9b3aebdeb340dd98d5800122a7a7
abu-hanzla <hanzla611@gmail.com>
2024-06-10 16:19:20 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,14 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
+         Hello World
         </div>
 
         <div className={styles.grid}>
